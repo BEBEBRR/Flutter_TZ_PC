cf7065a5badbe00c01c3edb7b0da5f88d837cc3b
BEBEBRR <mlbbmoba234@gmail.com>
2026-06-22 06:35:14 +0500

    feat: add ServicePopup widget to display detailed service information

---
+++ lib/widgets/service_popup.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../models/service_item.dart';
+
+class ServicePopup extends StatelessWidget {
+  final ServiceItem service;
+
+  const ServicePopup({Key? key, required this.service}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      tween: Tween(begin: 0.0, end: 1.0),
+      duration: const Duration(milliseconds: 150),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Transform.translate(
+            offset: Offset(0, 10 * (1 - value)),
+            child: child,
+          ),
+        );
+      },
+      child: Container(
+        width: 350,
+        padding: const EdgeInsets.all(28.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              blurRadius: 30,
+              offset: const Offset(0, 10),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              service.title.replaceAll('\n', ' ').toUpperCase(),
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w800,
+                letterSpacing: 0.5,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ...service.bullets
+                .map((bullet) => Padding(
+                      padding: const EdgeInsets.only(bottom: 12.0),
+                      child: InkWell(
+                        onTap: () => print('Клик: $bullet'),
+                        child: Text(
+                          bullet,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Color(0xFF333333),
+                            height: 1.4,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Color(0xFF999999),
+                          ),
+                        ),
+                      ),
+                    ))
+                .toList(),
+          ],
+        ),
+      ),
+    );
+  }
+}
