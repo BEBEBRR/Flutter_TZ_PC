1901bf7730d8ae5f1c2154465fc9d904b0c8ead8
BEBEBRR <mlbbmoba234@gmail.com>
2026-06-30 01:24:01 +0500

    refactor(ui): remove unused ghost logo widget

--- lib/widgets/logo_mark.dart
+++ lib/widgets/logo_mark.dart
@@ -10,37 +10,13 @@ class LogoMark extends StatelessWidget {
     return SizedBox(
       width: 190,
       height: 110,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Positioned(
-            left: 14,
-            top: 26,
-            child: Opacity(
-              opacity: 0.55,
-              child: Text(
-                'KAF\nSTUDIO',
-                style: TextStyle(
-                  fontFamily: AppTheme.fontFamily,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w800,
-                  height: 0.95,
-                  color: AppTheme.logoGhost,
-                  letterSpacing: 1,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            top: 0,
-            child: SvgPicture.asset(
-              'assets/icon_logo.svg',
-              width: 150,
-              alignment: Alignment.center,
-            ),
-          ),
-        ],
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: SvgPicture.asset(
+          'assets/icon_logo.svg',
+          width: 150,
+          alignment: Alignment.center,
+        ),
       ),
     );
   }
