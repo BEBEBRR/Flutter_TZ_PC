8fb62513ad8e11b8ac320d7bd66af61b5b88b8f2
BEBEBRR <mlbbmoba234@gmail.com>
2026-06-30 02:18:10 +0500

    feat(ui): add smooth entrance animation and tactile ripple effect to ServicePopup

--- lib/widgets/service_popup.dart
+++ lib/widgets/service_popup.dart
@@ -4,18 +4,19 @@ import '../models/service_item.dart';
 class ServicePopup extends StatelessWidget {
   final ServiceItem service;
 
-  const ServicePopup({Key? key, required this.service}) : super(key: key);
+  const ServicePopup({super.key, required this.service});
 
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder<double>(
       tween: Tween(begin: 0.0, end: 1.0),
-      duration: const Duration(milliseconds: 150),
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOutCubic,
       builder: (context, value, child) {
         return Opacity(
           opacity: value,
           child: Transform.translate(
-            offset: Offset(0, 10 * (1 - value)),
+            offset: Offset(0, 25 * (1 - value)),
             child: child,
           ),
         );
@@ -50,16 +51,23 @@ class ServicePopup extends StatelessWidget {
             ...service.bullets
                 .map((bullet) => Padding(
                       padding: const EdgeInsets.only(bottom: 12.0),
-                      child: InkWell(
-                        onTap: () => print('Клик: $bullet'),
-                        child: Text(
-                          bullet,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Color(0xFF333333),
-                            height: 1.4,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Color(0xFF999999),
+                      child: Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () => print('Клик: $bullet'),
+                          borderRadius: BorderRadius.circular(4),
+                          child: Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Text(
+                              bullet,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Color(0xFF333333),
+                                height: 1.4,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Color(0xFF999999),
+                              ),
+                            ),
                           ),
                         ),
                       ),
